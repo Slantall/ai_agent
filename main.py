3f80d80eebb814dea61554c089c3c5005dc15958
Slantall <slantallsstuff@gmail.com>
2025-06-16 12:38:31 -0700

    added AI call

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+if len(sys.argv) < 2:
+    print("Please provide a prompt after the command")
+    exit(code = 1)
+
+
+content = sys.argv[1]
+response = client.models.generate_content(model='gemini-2.0-flash-001',contents=content)
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
